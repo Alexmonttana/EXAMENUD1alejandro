1033d754900cef4a370deb44dc4869b1c12b60b8
LAPTOP-O3VOI188\alexp <alexperse7@hotmail.com>
2024-10-04 19:42:01 +0200

    Examen programacion

---
+++ examen_alejando2/src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner entrada=new Scanner(System);
+        int numero1=entrada.nextInt()
+
+        System.out.println("introduce un numero:");
+        while (numero1
+
+        System.out.println("introduce un numero:");
+
+
+
+
+
+
+
+
+        }
+    }
+}
